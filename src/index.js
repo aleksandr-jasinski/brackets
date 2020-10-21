86e2ca603891f78b1c83f4cf941e912bad568c5d
Aleksandr Jasinski <aleksandr.jasinski@gmail.com>
2020-10-21 12:12:55 +0300

    feat: pass 12 of 20 tests

--- src/index.js
+++ src/index.js
@@ -1,7 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  if (str.length % 2 === 1) {
-      return false;
-  }
-  isCorrect = true;
-  return isCorrect;
+let isCorrect;
+    if (str.length % 2 === 1) {
+        isCorrect = false;
+    } else {
+        const stack = [];
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            //??? config2 = [['(', ')'], ['[', ']']];
+            let opening = bracketsConfig[i][0];
+            let closing = bracketsConfig[i][1];
+            //TODO | = (|, |)
+            //??? '[(])'
+            for (let char of str) {
+                if (char === opening) {
+                    stack.push(char);
+                } else if (char === closing) {
+                    if (stack.length === 0) {
+                        return false;
+                    } else if (stack[stack.length - 1] === opening) {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        if (stack.length === 0) {
+        isCorrect = true;
+        } else {
+        console.log(stack.length);
+        console.log(stack);
+        isCorrect = false;
+        }
+
+    }
+return isCorrect;
 }
